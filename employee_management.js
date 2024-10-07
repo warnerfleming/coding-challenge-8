6bf40d455d1d92ed469e224b2c0bf233389663c4
warnerfleming <fleming14@usf.edu>
2024-10-06 21:39:42 -0400

    Create Employee class with properties and methods

--- employee_management.js
+++ employee_management.js
@@ -1,1 +1,16 @@
 
+//Task 1
+
+class Employee {
+     constructor(name, position, salary, department) {
+       this.name = name;
+      this.position = position;
+     this.salary = salary;
+     this.department = department
+  }
+  getDetails(){
+    console.log(`${this.name} position is ${this.position} and their salary
+        is ${this.salary}`)
+  }
+    
+}
