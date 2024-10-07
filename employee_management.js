c02c909f36864028e2c0e95fedde8ea124e51c10
warnerfleming <fleming14@usf.edu>
2024-10-06 22:18:22 -0400

    Create Department class to manage employees and calculate salary

--- employee_management.js
+++ employee_management.js
@@ -14,3 +14,21 @@ class Employee {
   }
     
 }
+
+//Task 2
+class Department {
+    constructor(name, employees) {
+      this.name = name;
+     this.employees = []
+ }
+ addEmployee(employee){
+this.employees.push(employee)
+ }
+ getDepartmentSalary(){
+    department.reduce((total, salary) => total + salary, 0)
+ }
+}
+
+const engineering = new Department("Engineering");
+
+const marketing = new Department("Marketing");
